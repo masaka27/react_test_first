0f51662a0f5dcaf6a87753c03306d8852085be37
masaka27 <15242577+masaka27@users.noreply.github.com>
2021-08-22 16:11:11 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import ColofulMessage from "./compornents/ColofulMessage";
+const App = () => {
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>Hello</h1>
+      <ColofulMessage color="blue">お元気ですか？</ColofulMessage>
+      <ColofulMessage color="pink">元気です</ColofulMessage>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
+    </>
+  );
+};
+
+export default App;
